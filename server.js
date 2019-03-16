6f0b2aafef32f437d8291269bb429a7d16794db4
sayex <elsayer@msn.com>
2019-03-16 14:37:03 -0600

    lint issue

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var logger = require("morgan");
 
-
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
@@ -25,19 +24,19 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.engine(
-    "handlebars",
-    exphbs({
-      defaultLayout: "main"
-    })
-  );
-  app.set("view engine", "handlebars");
-  
-  // Routes
-  require("./routes/apiRoutes")(app);
-  require("./routes/htmlRoutes")(app);
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
+  console.log("App running on port " + PORT + "!");
+});
 
 module.exports = app;
