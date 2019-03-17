4d71b9cc1594a10f4bdb0c4f81880dd8a13f36f8
sayex <elsayer@msn.com>
2019-03-16 22:28:49 -0600

    added api routes and database statments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,13 +8,49 @@ var MONGODB_URI =
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
-  // db.Article.create(result)
-  //   .then(function(dbArticle) {
-  //     // View the added result in the console
-  //     console.log(dbArticle);
-  //   })
-  //   .catch(function(err) {
-  //     // If an error occurred, log it
-  //     console.log(err);
-  //   });
+  app.get("/api/articles", function(req, res) {
+    db.Article.find({}).then(function(dbArticle) {
+      hbsObject = {
+        data: dbArticle
+      };
+      console.log(hbsObject);
+      res.render("articles", hbsObject);
+    });
+  });
+
+  app.get("/api/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .then(function(dbArticle) {
+        hbsObject = {
+          data: [dbArticle]
+        };
+        console.log(hbsObject);
+        res.render("articles", hbsObject);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/articles", function(req, res) {
+    db.Article.create(req.body)
+      .then(function(dbArticle) {
+        res.render("articles", dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, log it
+        console.log(err);
+      });
+  });
+
+  app.put("/api/articles", function(req, res) {
+    db.Article.create(req.body)
+      .then(function(dbArticle) {
+        res.render("articles", dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, log it
+        console.log(err);
+      });
+  });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,14 @@ var cheerio = require("cheerio");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    // First, we grab the body of the html with axios
     axios.get("http://www.echojs.com/").then(function(response) {
       var scrapData = {
         data: []
       };
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
-      // Now, we grab every h2 within an article tag, and do the following:
       // eslint-disable-next-line no-unused-vars
       $("article h2").each(function(i, element) {
-        // Save an empty result object
         var result = {};
-        // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
           .children("a")
           .text();
@@ -25,7 +20,9 @@ module.exports = function(app) {
         scrapData.data.push(result);
       });
       res.render("index", scrapData);
-      // Send a message to the client
     });
   });
 };
+
+
+mongodb://heroku_1h3381g0:9uucucrrdkct5declf617hulk5@ds247944.mlab.com:47944/heroku_1h3381g0
